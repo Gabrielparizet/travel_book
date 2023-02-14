8a4e58f3c28054c67fd02e0ec7f6c5afe26f57c9
tazkeyaislam <114977400+tazkeyaislam@users.noreply.github.com>
2023-02-14 14:27:51 +0100

    (feat) update news.php

--- news.php
+++ news.php
@@ -26,7 +26,8 @@
         $laQuestionEnSql = "
             SELECT posts.content,
             posts.created,
-            users.alias as author_name,  
+            users.alias as author_name, 
+            users.id as user_id, 
             count(likes.id) as like_number,  
             GROUP_CONCAT(DISTINCT tags.label) AS taglist 
             FROM posts
@@ -49,16 +50,16 @@
         }
 
         // Création des articles
-        while ($post = $lesInformations->fetch_assoc())
-        {
+        while ($post = $lesInformations->fetch_assoc()) { 
+            // echo "<pre>" . print_r($post, 1) . "</pre>";
             ?>
             <article>
                 <h3>
                     <time><?php echo $post['created'] ?></time>
                 </h3>
-                <address><?php echo $post['author_name'] ?></address>
+                <address><a href="wall.php?user_id=<?php echo $post['user_id'] ?>"><?php echo $post['author_name'] ?></a></address>
                 <div>
-                    <p><?php echo $post['content'] ?></p>
+                    <p><?php echo $post['content']?></p>
                 </div>
                 <footer>
                     <small>♥<?php echo $post['like_number'] ?></small>
