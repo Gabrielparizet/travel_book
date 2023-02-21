a964fc62266091302579027b542be92f4f1ba04f
21Gregor <bechetgregor5@gmail.com>
2023-02-21 10:47:11 +0100

    (feat)redirectiontonewswhenconnected

--- login.php
+++ login.php
@@ -56,6 +56,7 @@
                     // Etape 7 : Se souvenir que l'utilisateur s'est connecté pour la suite
                     // documentation: https://www.php.net/manual/fr/session.examples.basic.php
                     $_SESSION['connected_id']=$user['id'];
+                    header('Location: news.php');
                 }
             }
         }
